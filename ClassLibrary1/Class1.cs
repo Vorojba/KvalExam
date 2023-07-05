389e461203de42a19e8c2b469a4ca60b770d422a
316de <316de@315-DE-8>
2023-07-05 10:21:57 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    //Шрайвер Николас 31ИС
+    public class Building
+    {
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public List<Apartments> apartments = new List<Apartments>();
+
+        public Building(string address, string description)
+        {
+            Address = address;
+            Description = description;
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine($"Адрес:{Address}\nОписание:{Description}\nКвартира");
+            apartments.Sort();
+            foreach ( Apartments apartments in apartments )
+            {
+                apartments.Show();
+            }
+        }
+    }
+    //Шрайвер Николас 31ИС
+    public class Apartments : IComparable<Apartments>
+    {
+        public int Number { get; set; }
+        public string Owner { get; set; }
+        public Apartments(int number, string owner)
+        {
+            Number = number;
+            Owner = owner;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"\tНомер:{Number}\n\tВладелец:{Owner}");
+        }
+        //Шрайвер Николас 31ИС
+        public int CompareTo(Apartments other)
+        {
+            return Number.CompareTo(other.Number);
+        }
+    }
+
+}
---
+++ kvall/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kvall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Шрайвер Николас 31ИС
+            Building building = new Building("ул. Долгопрудная д.5", "«Мытищи Lite» – новое место для жизни, в котором свободно и комфортно.");
+            building.apartments.Add(new Apartments(7, "Александр Николаевич Журавлев"));
+            building.apartments.Add(new Apartments(45, "Михаил Владимирович Цветков"));
+            building.apartments.Add(new Apartments(23, "Данила Алексндрович Никитич"));
+            building.ShowAll();
+            Console.ReadKey();
+
+        }
+    }
+}
